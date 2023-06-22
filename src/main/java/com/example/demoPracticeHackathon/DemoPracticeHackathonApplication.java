96f963adba0e281f01dcc9767db7e57f6cdc21e6
anishgh94 <anishgh94@gmail.com>
2023-06-21 20:18:25 -0400

    First Commit

---
+++ src/main/java/com/example/demoPracticeHackathon/DemoPracticeHackathonApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoPracticeHackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPracticeHackathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPracticeHackathonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoPracticeHackathon/DemoPracticeHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoPracticeHackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPracticeHackathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
